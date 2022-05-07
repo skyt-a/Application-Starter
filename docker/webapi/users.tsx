f59a89673a5609275d30f372454cd5e44294db17
RinGoku <yuyu80105@gmail.com>
2022-05-07 21:43:52 +0900

    fix

--- docker/webapi/users.tsx
+++ docker/webapi/users.tsx
@@ -1,15 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 const test = () => {
-    const [users, setUsers] = useState([]);
-    useEffect(() => {
-        fetch("http://localhost:3000/users")
-            .then((response) => response.json())
-            .then(setUsers)
-    }, [])
-    return (<div>
-        {users.map(user => <div>{user.name}:{user.email}</div>)}
-    </div>)
-}
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:3000/users')
+      .then((response) => response.json())
+      .then(setUsers);
+  }, []);
+  return (
+    <div>
+      {users.map((user) => (
+        <div>
+          {user.name}:{user.email}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default test;
